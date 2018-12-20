230022f90f5cbfed5a01c6e1e2b44f02e986f34f
NastiaPolukhina1991 <39697576+NastiaPolukhina1991@users.noreply.github.com>
2018-12-20 18:30:11 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,20 @@ window.onload = function () {
 
   function toggleNav() {
     let navMain = document.querySelector('.page-header');
-    navMain.classList.toggle('page-header--opened')
+    navMain.classList.toggle('page-header--opened');
   }
 
   navToggle.addEventListener('click', toggleNav, false);
   navCloseItem.addEventListener('click', toggleNav, false);
+
+
+  window.onscroll = function () {
+    const navMain = document.querySelector('.page-header');
+    const mq = window.matchMedia("(max-width: 679px)");
+    if (mq.matches) {
+      navMain.classList.remove('page-header--opened');
+    } else {
+      return;
+    }
+  };
 };
